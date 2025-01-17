97f8a27c650f65b2b06355b1e19444f9da6745f6
Emmanuelomolaju <emmanuelomolaju964@gmail.com>
2025-01-17 15:11:11 +0100

    fix: update team member images in Team section

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -21,19 +21,19 @@ const TeamSection = () => {
   const teamMembers = [
     {
       name: "Chuma Igwe",
-      image: "img/team/avatar.webp",
+      image: "img/team/chuma.jpg",
       description:
         "Chuma Igwe holds a master's degree in marketing from the University that is famed for the most prestigious Marketing Faculty in Europe: The University of Strathclyde Glasgow in 1980. Thereafter he joined Nigerian Breweries where he garnered a distinguished career in very senior positions spanning from Sales, Distribution, Marketing and Corporate Communications.",
     },
     {
       name: "Damian Wilson Nwatarali",
-      image: "img/team/avatar.webp",
+      image: "img/team/damian.jpg",
       description:
         "Nwatarali is a graduate of the University of Ibadan, Heriot-Watt University, Edunburgh and University of Uyo. A fellow of African Centre for supply chain management and senior parent of The NSE-ASI securities. He has also attended executive development programmes of both local and international institutions including the prestigious London Business School.",
     },
     {
       name: "Lucky Nwachukwu",
-      image: "img/team/avatar.webp",
+      image: "img/team/lucky.jpg",
       description:
         "He is a graduate of Business Management from the University of Nigeria. He had a brief stint with Central Bank of Nigeria before joining the Management cadre of Nigerian Breweries plc. He has specialist background in Logistics, Communication and General Management.",
     },
